4c1eeb0507db838ee7819f2e6046683f0db7c5b7
Pham Ngoc Kien <kienpndev@gmail.com>
2023-11-16 15:42:55 +0700

    Done Login

---
+++ app/src/main/java/com/example/hucegym/LoginViewModel.java
@@ -0,0 +1,16 @@
+package com.example.hucegym;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.widget.TextView;
+import androidx.databinding.BaseObservable;
+
+
+
+public class LoginViewModel extends BaseObservable {
+
+
+
+
+}
--- app/src/main/java/com/example/hucegym/MainActivity.java
+++ app/src/main/java/com/example/hucegym/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.hucegym;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtQuenMatKhau;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        init();
     }
+    //Khởi tạo các thành phần
+    public void init(){
+        try{
+            txtQuenMatKhau = findViewById(R.id.txtQuenMatKhau);
+            String htmlcontent = "<u>Quên mật khẩu</u>";
+            txtQuenMatKhau.setText(Html.fromHtml(htmlcontent));
+        }catch (Exception ex)
+        {
+            Log.e("Init", ex.getMessage());
+        }
+    }
+
+    // Xử lý sự kiện
 }
