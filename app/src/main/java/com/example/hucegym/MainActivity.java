820bdb760beb509be31d8341748269c3988bc934
Pham Ngoc Kien <kienpndev@gmail.com>
2023-12-07 20:58:41 +0700

    Done Đăng ký và trang chủ

--- app/src/main/java/com/example/hucegym/MainActivity.java
+++ app/src/main/java/com/example/hucegym/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_trang_chu);
         init();
     }
     //Khởi tạo các thành phần
