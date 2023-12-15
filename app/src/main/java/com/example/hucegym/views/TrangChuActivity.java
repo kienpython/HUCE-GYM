11798e116b4d9c151414653bcd734b8a80bc198f
Pham Ngoc Kien <kienpndev@gmail.com>
2023-12-15 22:14:40 +0700

    Done phác thảo calander và feedback

--- app/src/main/java/com/example/hucegym/views/TrangChuActivity.java
+++ app/src/main/java/com/example/hucegym/views/TrangChuActivity.java
@@ -2,10 +2,12 @@ package com.example.hucegym.views;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hucegym.CheckCalendarActivity;
 import com.example.hucegym.viewmodel.TrangChuViewModel;
 import com.example.hucegym.R;
 
@@ -14,59 +16,14 @@ public class TrangChuActivity extends AppCompatActivity {
     private TrangChuViewModel viewModel;
     private ImageButton ibtnRules;
     private ImageButton ibtnFeedBack;
+    private Button btnCheckCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_chu);
         feedBack();
-//        ibtnRules = findViewById(R.id.ibtnDieuKhoan);
-//        // Bắt sự kiện khi điều khoản được click
-//        ibtnRules.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startRulesActivity();
-//            }
-//        });
-
-//
-//        viewModel = new TrangChuViewModel();
-//
-//        ImageButton xemLichButton = findViewById(R.id.button5);
-//        ImageButton theoDoiSucKhoeButton = findViewById(R.id.button3);
-//        ImageButton xemBaiTapButton = findViewById(R.id.button4);
-//        //Thêm ImageButton và TextView khác tương tự
-//
-//        TextView trangChuTextView = findViewById(R.id.textView);
-//        TextView xemLichTextView = findViewById(R.id.textView24);
-//        TextView theoDoiSucKhoeTextView = findViewById(R.id.textView3);
-//        //Thêm TextView khác tương tự
-//
-//        // Bắt sự kiện khi Xem Lịch được click
-//        xemLichButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                viewModel.handleXemLich(TrangChuActivity.this);
-//            }
-//        });
-//
-//        // Bắt sự kiện khi Theo Dõi Sức Khỏe được click
-//        theoDoiSucKhoeButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                viewModel.handleTheoDoiSucKhoe(TrangChuActivity.this);
-//            }
-//        });
-//
-//        // Bắt sự kiện khi Xem Bài Tập được click
-//        xemBaiTapButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                viewModel.handleXemBaiTap(TrangChuActivity.this);
-//            }
-//        });
-//
-//        //Thêm sự kiện cho các ImageButton và TextView khác
+        checkCalendar();
     }
 
     // Chuyển trang sang phần điều khoản
@@ -87,6 +44,23 @@ public class TrangChuActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startFeedBackActivity();
+            }
+        });
+    }
+
+    // Chuyển sang phần góp ý
+    private void startCheckCalendarActivity() {
+        Intent intent = new Intent(TrangChuActivity.this, CheckCalendarActivity.class);
+        startActivity(intent);
+    }
+    // Xử lý sự kiện nút góp ý
+    private void checkCalendar(){
+        btnCheckCalendar = findViewById(R.id.btnCheckCalendar);
+        btnCheckCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startCheckCalendarActivity();
             }
         });
     }
