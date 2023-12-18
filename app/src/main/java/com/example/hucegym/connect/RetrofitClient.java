8dcfd3e9b64036adbce4e0c8af47b8fc170ddf5c
Tung <lmt3997@gmail.com>
2023-12-18 22:53:12 +0700

    .

--- app/src/main/java/com/example/hucegym/connect/RetrofitClient.java
+++ app/src/main/java/com/example/hucegym/connect/RetrofitClient.java
@@ -1,17 +1,27 @@
 package com.example.hucegym.connect;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static Retrofit retrofit;
-    private static final String BASE_URL = "http://localhost/login.php";
+    public static String Base_url = "http://10.0.2.2/";
 
-    public static Retrofit getInstance() {
-        if (retrofit == null) {
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetroClient(){
+        if(retrofit == null){
+            OkHttpClient.Builder okBuilder = new OkHttpClient.Builder()
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .connectTimeout(30 , TimeUnit.SECONDS)
+                    .retryOnConnectionFailure(true);
+
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(Base_url)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okBuilder.build())
                     .build();
         }
         return retrofit;
--- app/src/main/java/com/example/hucegym/model/User.java
+++ app/src/main/java/com/example/hucegym/model/User.java
@@ -3,11 +3,14 @@ package com.example.hucegym.model;
 public class User {
     private String username;
     private String password;
+    private String name;
     private String chuc_vu;
 
-    public User(String username, String password) {
+    public User(String username, String password, String name) {
         this.username = username;
         this.password = password;
+        this.name = name;
+        this.chuc_vu = "Hội Viên";
     }
 
     public String getUsername() {
@@ -26,11 +29,19 @@ public class User {
         this.password = password;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getChuc_vu() {
         return chuc_vu;
     }
+
     public void setChuc_vu(String chuc_vu) {
         this.chuc_vu = chuc_vu;
     }
 }
-
